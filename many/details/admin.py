83e54c41852692c964cac428a173ef41ce2ed238
Navya Dommeti <srinavya.d@gmail.com>
2016-12-18 18:40:00 +0530

    updated ordering

--- many/details/admin.py
+++ many/details/admin.py
@@ -9,14 +9,18 @@ from .models import Author,Book,Membership
 class MembershipInline(SortableInlineAdminMixin,admin.TabularInline): #SortableInlineAdminMixin
 	model = Membership
 	fields = ['author','book','my_order']
-
+	
 
 class BookAdmin(SortableAdminMixin,admin.ModelAdmin):
-	inlines = (MembershipInline,)	
-		
+	list_displayst_editable = ('authors', 'title','my_order',)
+	list_display = ('title',)
+	inlines = (MembershipInline,)
+	
+
 		
 class AuthorAdmin(SortableAdminMixin,admin.ModelAdmin):
-	pass
+	list_displayst_editable = ('name','my_order',)
+	list_display = ('name',)
 
 admin.site.register(Book,BookAdmin)
 admin.site.register(Author,AuthorAdmin)
